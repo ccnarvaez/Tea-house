51634ff2a52fdea0c1920aa37583879ba6b716cf
Claudia Narvaez <ccnarvaez90@gmail.com>
2021-08-23 22:53:09 -0400

    order object

--- src/views/MenuGrid.js
+++ src/views/MenuGrid.js
@@ -1,25 +1,39 @@
+let order = [];
+    const SaveItems = ( e )=> {
+        let index = order.findIndex( element => element.id === e.target.dataset.id );
+        if ( index === -1){
+            let item = {
+                id: e.target.dataset.id,
+                nombre: e.target.dataset.item,
+                precio: e.target.dataset.price,
+                cantidad: 1,
+            };
 
+            order.push( item );
+        }
+        else{            
+            order[index].cantidad += 1;
+        }
+    
+        
+        console.log(order);
 
-
-export const MenuGrid = ( { id, item, price, img }) => {
-
-    const SaveItems = (e) => {
-
-
-        return (
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title"> {e.target.dataset.item} </h5>
-                    <h6 class="card-subtitle mb-2 text-muted"> {e.target.dataset.item} </h6>
-                    <input></input>
-                </div>
-            </div>
-        )
+        // return (
+            
+        //     <div class="card">
+        //         <div class="card-body">
+        //             <h5 class="card-title"> {e.target.dataset.item} </h5>
+        //             <h6 class="card-subtitle mb-2 text-muted"> {e.target.dataset.item} </h6>
+        //             <input></input>
+        //         </div>
+        //     </div>
+        // )
     
     };
 
-  
 
+export const MenuGrid = ( { id, item, price, img }) => {
+    
     return(  
 
         <div className="card" key = { id }>
@@ -29,9 +43,11 @@ export const MenuGrid = ( { id, item, price, img }) => {
                 <h4 className="card-text"> { price } $ </h4>
                 <button 
                     className="btn btn-danger btn-sm" 
-                    onClick= { SaveItems  } 
+                    onClick= { SaveItems } 
                     data-item = { item } 
-                    data-price = { price }> 
+                    data-price = { price }
+                    data-id = { id }
+                    > 
                         Agregar 
                 </button>
             </div>
